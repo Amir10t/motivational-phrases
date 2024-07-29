dd2aa023abd9db023d2780add938f25dfb37b7be
Amir Talebi <amirhasantalebi1400@gmail.com>
2024-07-29 15:46:25 +0330

    Add script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+change_background_color()
+var data = JSON.parse("");
+phrase_id = Math.floor(Math.random() * data.length);
+var phrase_h1 = document.getElementById("phrase")
+phrase_h1.innerHTML = data[phrase_id][1]
+function pervious_phrase() {
+    if (phrase_id!=0) {
+        phrase_id -= 1
+        phrase_h1.innerHTML = data[phrase_id][1]
+        change_background_color()
+    } else {
+        phrase_id = data.length
+        phrase_h1.innerHTML = data[phrase_id][1]
+        change_background_color()
+    }
+}
+function next_phrase() {
+    if (phrase_id!=data.length) {
+        phrase_id += 1
+        phrase_h1.innerHTML = data[phrase_id][1]
+        change_background_color()
+    } else {
+        phrase_id = 0
+        phrase_h1.innerHTML = data[phrase_id][1]
+        change_background_color()
+    }
+}
+function change_background_color() {
+    random_background_color_id = Math.floor(Math.random() * 10);
+    const background_colors = ["aquamarine", "red", "orange",
+        "yellowgreen", "green","blue", "pink", "black", "purple", "gray"]
+    var body = document.getElementById("body")
+    body.style = "background-color:"+background_colors[random_background_color_id]+";"
+}
